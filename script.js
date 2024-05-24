009c49e61af93059b2d47484aba8a4062cbf51dc
charliephelpscodes <164572692+charliephelpscodes@users.noreply.github.com.>
2024-05-24 16:54:17 -0700

    Create button container and all buttons in DOM

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 //Variable Assignment
 GRIDSIDE = 500;
 
+//DOM Nodes
+let buttonContainer = document.createElement('div');
+buttonContainer.classList.add('buttonContainer');
+const buttonContainerSelector = document.querySelector('.buttonContainer');
+document.body.prepend(buttonContainer);
+
+let rnbwButton = document.createElement('button');
+rnbwButton.classList.add('rnbwButton');
+rnbwButton.classList.add('button');
+const rnbwButtonSelector = document.querySelector('.rnbwButton');
+rnbwButton.textContent = 'Rainbow Button';
+buttonContainer.appendChild(rnbwButton);
+
+let shaderButton = document.createElement('button');
+shaderButton.classList.add('shaderButton');
+shaderButton.classList.add('button');
+const shaderButtonSelector = document.querySelector('.shaderButton');
+shaderButton.textContent = 'Shader';
+buttonContainer.appendChild(shaderButton);
+
+let button = document.createElement('button');
+button.classList.add('button');
+const clearButtonSelector = document.querySelector('.button');
+button.textContent = 'Resize the Grid!';
+buttonContainer.appendChild(button);
+
 //Selectors
 let container = document.querySelector(".container");
 
